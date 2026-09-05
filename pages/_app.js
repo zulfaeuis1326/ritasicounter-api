f735c1c97f077a012d378003291edb67704413bc
zulfaeuis1326 <zulfaeuis1326@gmail.com>
2026-09-06 07:31:13 +0800

    Add viewport meta tag to _app.js
    Add viewport meta tag for responsive design.

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
